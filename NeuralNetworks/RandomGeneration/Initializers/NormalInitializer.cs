60a9cf04356da7e656b365026ec40455b9f5b2fa
Károly Iván <ivankaroly94@gmail.com>
2023-10-20 13:13:55 +0200

    Add normal and uniform initializers

---
+++ NeuralNetworks/RandomGeneration/Initializers/NormalInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Ivankarez.NeuralNetworks.Abstractions;
+
+namespace Ivankarez.NeuralNetworks.RandomGeneration.Initializers
+{
+    public class NormalInitializer : IInitializer
+    {
+        public IRandomProvider RandomProvider { get; }
+        public float Mean { get; }
+        public float StdDev { get; }
+
+        public NormalInitializer(IRandomProvider randomProvider, float mean, float stdDev)
+        {
+            RandomProvider = randomProvider;
+            Mean = mean;
+            StdDev = stdDev;
+        }
+
+        public float GenerateValue(int fanIn, int fanOut)
+        {
+            return RandomProvider.NextNormalFloat(Mean, StdDev);
+        }
+    }
+}
---
+++ NeuralNetworks/RandomGeneration/Initializers/UniformInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Ivankarez.NeuralNetworks.Abstractions;
+
+namespace Ivankarez.NeuralNetworks.RandomGeneration.Initializers
+{
+    internal class UniformInitializer : IInitializer
+    {
+        public IRandomProvider RandomProvider { get; }
+        public float Min { get; }
+        public float Max { get; }
+
+        public UniformInitializer(IRandomProvider randomProvider, float min, float max)
+        {
+            RandomProvider = randomProvider;
+            Min = min;
+            Max = max;
+        }
+
+        public float GenerateValue(int fanIn, int fanOut)
+        {
+            return RandomProvider.NextFloat(Min, Max);
+        }
+    }
+}
