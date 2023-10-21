4952bdda6f374057d11f050b35d0155b7eed874d
Károly Iván <ivankaroly94@gmail.com>
2023-10-21 11:50:51 +0200

    Add initializer support for RecurrentLayer

--- NeuralNetworks.Test/Layers/RecurrentLayerTests.cs
+++ NeuralNetworks.Test/Layers/RecurrentLayerTests.cs
@@ -1,6 +1,8 @@
 ﻿using FluentAssertions;
+using Ivankarez.NeuralNetworks.Abstractions;
 using Ivankarez.NeuralNetworks.Activations;
 using Ivankarez.NeuralNetworks.Layers;
+using Ivankarez.NeuralNetworks.RandomGeneration.Initializers;
 using Ivankarez.NeuralNetworks.Utils;
 using NUnit.Framework;
 
@@ -8,10 +10,12 @@ namespace Ivankarez.NeuralNetworks.Test.Layers
 {
     public class RecurrentLayerTests
     {
+        private static readonly IInitializer defaultInitializer = new ZerosInitializer();
+
         [Test]
         public void TestUpdate_SingleCall()
         {
-            var layer = new RecurrentLayer(2, new LinearActivation(), false);
+            var layer = new RecurrentLayer(2, new LinearActivation(), false, defaultInitializer, defaultInitializer, defaultInitializer);
             layer.Build(1);
             layer.Parameters.Get2dVector("weights").Fill(new float[,] { { 1 }, { -1 } });
             layer.Parameters.Get1dVector("recurrentWeights").Fill(.5f, -.5f);
@@ -27,7 +31,7 @@ namespace Ivankarez.NeuralNetworks.Test.Layers
         [Test]
         public void TestUpdate_DoubleCall()
         {
-            var layer = new RecurrentLayer(2, new LinearActivation(), false);
+            var layer = new RecurrentLayer(2, new LinearActivation(), false, defaultInitializer, defaultInitializer, defaultInitializer);
             layer.Build(1);
             layer.Parameters.Get2dVector("weights").Fill(new float[,] { { 1 }, { -1 } });
             layer.Parameters.Get1dVector("recurrentWeights").Fill(.5f, -.5f);
@@ -45,7 +49,7 @@ namespace Ivankarez.NeuralNetworks.Test.Layers
         [Test]
         public void TestUpdate_HappyPathWithBias()
         {
-            var layer = new RecurrentLayer(2, new LinearActivation(), true);
+            var layer = new RecurrentLayer(2, new LinearActivation(), true, defaultInitializer, defaultInitializer, defaultInitializer);
             layer.Build(1);
             layer.Parameters.Get2dVector("weights").Fill(new float[,] { { 1 }, { -1 } });
             layer.Parameters.Get1dVector("recurrentWeights").Fill(.5f, -.5f);
@@ -58,6 +62,21 @@ namespace Ivankarez.NeuralNetworks.Test.Layers
             result[1].Should().Be(9f);
 
             layer.State.Get1dVector("nodeValues").Should().HaveCount(2);
+        }
+
+        [Test]
+        public void TestBuild_UseInitializers()
+        {
+            var kernelInitializer = new ConstantInitializer(1);
+            var recurrentInitializer = new ConstantInitializer(2);
+            var biasInitializer = new ConstantInitializer(3);
+
+            var layer = new RecurrentLayer(2, new LinearActivation(), true, kernelInitializer, biasInitializer, recurrentInitializer);
+            layer.Build(1);
+
+            layer.Parameters.Get2dVector("weights").Should().BeEquivalentTo(new float[,] { { 1 }, { 1 } });
+            layer.Parameters.Get1dVector("recurrentWeights").Should().BeEquivalentTo(new float[] { 2, 2 });
+            layer.Parameters.Get1dVector("biases").Should().BeEquivalentTo(new float[] { 3, 3 });
         }
     }
 }
--- NeuralNetworks/Layers/RecurrentLayer.cs
+++ NeuralNetworks/Layers/RecurrentLayer.cs
@@ -1,4 +1,5 @@
 ﻿using Ivankarez.NeuralNetworks.Abstractions;
+using Ivankarez.NeuralNetworks.RandomGeneration;
 using Ivankarez.NeuralNetworks.Values;
 using System;
 
@@ -7,7 +8,9 @@ namespace Ivankarez.NeuralNetworks.Layers
     public class RecurrentLayer : IModelLayer
     {
         public int NodeCount { get; }
-
+        public IInitializer KernelInitializer { get; }
+        public IInitializer BiasInitializer { get; }
+        public IInitializer RecurrentInitializer { get; }
         public NamedVectors<float> Parameters { get; }
         public NamedVectors<float> State { get; }
 
@@ -19,23 +22,29 @@ namespace Ivankarez.NeuralNetworks.Layers
         private float[] nodeValues;
         private float[] biases;
 
-        public RecurrentLayer(int nodeCount, IActivation activation, bool useBias)
+        public RecurrentLayer(int nodeCount, IActivation activation, bool useBias, IInitializer kernelInitializer, IInitializer biasInitializer, IInitializer recurrentInitializer)
         {
             if (nodeCount <= 0) throw new ArgumentOutOfRangeException(nameof(nodeCount), "Must be bigger than zero");
             if (activation == null) throw new ArgumentNullException(nameof(activation));
+            if (kernelInitializer == null) throw new ArgumentNullException(nameof(kernelInitializer));
+            if (biasInitializer == null) throw new ArgumentNullException(nameof(biasInitializer));
+            if (recurrentInitializer == null) throw new ArgumentNullException(nameof(recurrentInitializer));
 
             NodeCount = nodeCount;
             this.activation = activation;
             this.useBias = useBias;
+            KernelInitializer = kernelInitializer;
+            BiasInitializer = biasInitializer;
+            RecurrentInitializer = recurrentInitializer;
             Parameters = new NamedVectors<float>();
             State = new NamedVectors<float>();
         }
 
         public void Build(int inputSize)
         {
-            weights = new float[NodeCount,inputSize];
-            recurrentWeights = new float[NodeCount];
-            biases = new float[useBias ? NodeCount : 0];
+            weights = KernelInitializer.GenerateValues2d(inputSize, NodeCount, NodeCount, inputSize);
+            recurrentWeights = RecurrentInitializer.GenerateValues(inputSize, NodeCount, NodeCount);
+            biases = useBias ? BiasInitializer.GenerateValues(inputSize, NodeCount, NodeCount) : new float[0];
             nodeValues = new float[NodeCount];
 
             State.Add("nodeValues", nodeValues);
