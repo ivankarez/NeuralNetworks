7766790c10b0c1a060b3f6098e21f591733bdcd6
Károly Iván <ivankaroly94@gmail.com>
2023-10-21 11:58:49 +0200

    Add Tanh activation

---
+++ NeuralNetworks.Test/Activations/TanhActivationTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Ivankarez.NeuralNetworks.Activations;
+using NUnit.Framework;
+
+namespace Ivankarez.NeuralNetworks.Test.Activations
+{
+    public class TanhActivationTests
+    {
+        [Test]
+        public void TestApply_HappyPath()
+        {
+            var activation = new TanhActivation();
+            var result = activation.Apply(0.5f);
+            result.Should().BeApproximately(0.462117157f, 0.0001f);
+        }
+
+        [Test]
+        public void TestApply_NegativeInput()
+        {
+            var activation = new TanhActivation();
+            var result = activation.Apply(-0.5f);
+            result.Should().BeApproximately(-0.462117157f, 0.0001f);
+        }
+
+        [Test]
+        public void TestApply_ZeroInput()
+        {
+            var activation = new TanhActivation();
+            var result = activation.Apply(0f);
+            result.Should().BeApproximately(0f, 0.0001f);
+        }
+    }
+}
---
+++ NeuralNetworks/Activations/TanhActivation.cs
@@ -0,0 +1,13 @@
+﻿using Ivankarez.NeuralNetworks.Abstractions;
+using System;
+
+namespace Ivankarez.NeuralNetworks.Activations
+{
+    public class TanhActivation : IActivation
+    {
+        public float Apply(float input)
+        {
+            return (float)Math.Tanh(input);
+        }
+    }
+}
