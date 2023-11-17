c41598b577bb5912fffd6a146b057e7828a1eafe
Károly Iván <ivankaroly94@gmail.com>
2023-11-17 12:53:31 +0100

    Add layer benchmarks

---
+++ NeuralNetworks.Benchmarks/Layers/DenseLayerBenchmark.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using Ivankarez.NeuralNetworks.Api;
+using Ivankarez.NeuralNetworks.Layers;
+using Ivankarez.NeuralNetworks.RandomGeneration;
+
+namespace Ivankarez.NeuralNetworks.Benchmarks.Layers
+{
+    [MemoryDiagnoser(false)]
+    public class DenseLayerBenchmark
+    {
+        private readonly DenseLayer layer;
+        private readonly float[] input;
+
+        public DenseLayerBenchmark()
+        {
+            layer = NN.Layers.Dense(100);
+            layer.Build(NN.Size.Of(100));
+            input = NN.Random.Default().NextFloats(1f, 100);
+        }
+
+        [Benchmark]
+        public void Dense() => layer.Update(input);
+    }
+}
