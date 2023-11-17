b4422c291c8fe10e5118c5dceef8dcbf6a1b4043
Károly Iván <ivankaroly94@gmail.com>
2023-11-17 13:33:42 +0100

    Add benchmark for GRU layer

---
+++ NeuralNetworks.Benchmarks/Layers/GruLayerBenchmark.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using Ivankarez.NeuralNetworks.Api;
+using Ivankarez.NeuralNetworks.Layers;
+using Ivankarez.NeuralNetworks.RandomGeneration;
+
+namespace Ivankarez.NeuralNetworks.Benchmarks.Layers
+{
+    [MemoryDiagnoser(false)]
+    public class GruLayerBenchmark
+    {
+        private readonly GruLayer layer;
+        private readonly float[] input;
+
+        public GruLayerBenchmark()
+        {
+            layer = NN.Layers.GRU(NN.Size.Of(100));
+            layer.Build(NN.Size.Of(100));
+            input = NN.Random.Default().NextFloats(1f, 100);
+        }
+
+        [Benchmark]
+        public void GRU() => layer.Update(input);
+    }
+}
